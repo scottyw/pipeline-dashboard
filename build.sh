d934e570d365ca88315e44b172e9cb0ba233edf8
Mikker Gimenez-Peterson <mikker+05250246@puppet.com>
2019-10-08 13:33:10 -0700

    Create public folder when building

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ env GOOS=linux go build -o web cmd/web/main.go
 
 cd frontend; yarn build; cd ..
 
+mkdir public
 cp frontend/build/index.html public/index.html
 
 docker build --no-cache . -f Dockerfile -t gcr.io/infracore/ci-dashboard:$(git rev-parse --short HEAD)
